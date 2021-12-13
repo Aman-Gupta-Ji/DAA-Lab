df2d7031fb40553811a4caec16b010b967a5f164
Aman Gupta <amanguptaprogrammer@gmail.com>
2021-12-13 14:07:36 +0530

    Greedy

---
+++ Greedy/SmallestNonConstructibleValue.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+//we cannot creat a change amount greater then the sum of the coins we have
+int NoMinChange(vector<int> coins, int n) { //Smallest non constructable value
+    sort(coins.begin(),coins.end());
+    int max_construct=0;
+    for(int a : coins) {
+        if(a>max_construct+1)
+            break;
+        max_construct+=a;
+    }
+    return max_construct+1;
+}
+int main(void) {
+    int n, i, c;
+    cin>>n;
+    vector<int> coins;
+    for(i=0;i<n;i++) {
+        cin>>c;
+        coins.push_back(c);
+    }
+    int min=NoMinChange(coins,n);
+    cout<<min<<endl;
+}
